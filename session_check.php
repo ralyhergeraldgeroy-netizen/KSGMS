21874639ca033799cf3f2af0198c64a9dd7ae4c3
ralyher <ralyhergeraldgeroy@gmail.com>
2026-07-09 14:50:52 +0800

    Delete session_check.php

--- session_check.php
+++
@@ -1,7 +0,0 @@
-<?php
-$isAdmin = isset($_SESSION['is_admin']) &&
-           $_SESSION['is_admin'] === true;
-
-$isGuestLoggedIn = isset($_SESSION['guest_user']) &&
-                   !empty($_SESSION['guest_user']);
-?>
